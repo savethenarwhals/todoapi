f5ac3cc5bb3da5025ac8cc6e26dfd3060e23914d
jered <me@jered.dev>
2022-02-01 22:48:58 -0700

    Controller development.
    Finished Model and Controller development. Updated the DBContext and Controller to use a DTO instead of the entire object.

--- TodoApi/TodoApi/Program.cs
+++ TodoApi/TodoApi/Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+// In ASP.NET Core, services such as the DB context must be registered with the
+// dependency injection container. The container provides the service to controllers.
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
+//});
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,8 +26,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
 
 app.UseHttpsRedirection();
